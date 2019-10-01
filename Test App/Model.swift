036e7b3d9768187fac335b111e4f3cef459e8792
Kostia Kovba <kovbakostia@gmail.com>
2019-10-01 19:06:28 +0300

    1

---
+++ Test App/Model.swift
@@ -0,0 +1,11 @@
+//
+//  Model.swift
+//  Test App
+//
+//  Created by Konstantin Kovba on 9/26/19.
+//  Copyright © 2019 Konstantin Kovba. All rights reserved.
+//
+
+import Foundation
+
+var nameDudes: [String] = ["Ірина", "Діна", "Мальвіна", "Ромашка"]
